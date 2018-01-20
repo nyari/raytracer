9ab354ea2b608816eb57319e16afa47e2d1a4391
Nyári Dávid Tamás <nyaridavid@gmail.com>
2018-01-20 08:56:41 +0100

    Corrected issue where transformed RayIntersection didn't calculate distance correctly, resulting in unilluminated scaled model, and other issues when ModelViewModelWrapper was used.

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ use image::{DynamicImage, Rgba, Pixel, GenericImage, ImageFormat};
 
 
 fn main() {
-    let solid_shiny_red = Material::new_shiny(Color::new(0.87, 0.17, 0.08), (Color::new(1.0, 1.0, 1.0), 2.0), None);
+    let solid_shiny_red = Material::new_shiny(Color::new(0.87, 0.17, 0.08), (Color::new(1.0, 1.0, 1.0), 7.0), None);
     let sphere = {
         let mut result = ModelViewModelWrapper::new_identity(SolidUnitSphere::new(solid_shiny_red));
         result.scale_uniform(2.0);
         result
     };
 
-    let light = DotLightSource::new_natural(Color::one(), 50.0, Point3::new(0.0, 0.0, 8.0));
+    let light = DotLightSource::new_natural(Color::one(), 40.0, Point3::new(8.0, -8.0, 0.0));
 
     let intersector = SimpleIntersector::new(vec![Box::new(sphere)]);
     let illuminator = SimpleIlluminator::new(vec![Box::new(light)]);
@@ -34,6 +34,7 @@ fn main() {
 
     let mut result_image = DynamicImage::new_rgb8(screen_hor_res as u32, screen_ver_res as u32);
     for (ray, coord) in PortionableViewIterator::new(&view) {
+
         match world.cast_ray(&ray) {
             Some(color) => {
                 let (r, g, b) = color.normalized().mul_scalar(&(u8::max_value() as FloatType)).get();
